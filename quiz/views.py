9b1588f5d2f6eb33a81741c8f62135f94b7b4686
maria-selting <seltingmaria@gmail.com>
2015-08-06 11:17:26 +0200

    Added description in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,23 @@
+# coding: utf-8
+
 from django.shortcuts import render
+
+quizzes={
+	"movies":{
+		"name":u"Movies of the 80´s",
+		"description": "Can you recall what song belongs to what movie?"
+	},
+	"musicians":{
+		"name":u"Musicians",
+		"description":u"How well do you know the great musicians of the 80's?"
+
+	},
+	"fashion":{
+		"name":u"Fashion",
+		"description":u"Do you remember the fashion of the 80´s?"
+
+	},
+}
 
 # Create your views here.
 def startpage(request):
