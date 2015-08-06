61556b2a43f52b4c871274d8457aa29f8e64d6cc
maria-selting <seltingmaria@gmail.com>
2015-08-06 09:53:21 +0200

    Added files and installed app

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    'quiz',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
