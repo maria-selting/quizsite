39a09657710296695475d175ab73a86b9fc69e13
maria-selting <seltingmaria@gmail.com>
2015-08-07 10:04:52 +0200

    Added django link to quiz page

--- quiz/views.py
+++ quiz/views.py
@@ -13,8 +13,7 @@ def startpage(request):
 
 def quiz(request, slug):
 	context = {
-		"quiz": quizzes[slug], 
-		"quiz_slug": slug,
+		"quiz": Quiz.objects.get(slug=slug),
 	}
 	return render(request, "quiz/quiz.html", context)
 
